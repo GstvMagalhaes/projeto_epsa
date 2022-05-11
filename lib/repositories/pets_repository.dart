9af5cbf03bd5fd6c8664952337af875f94f6d2ea
xcaio <mrlose22@gmail.com>
2022-05-11 09:29:54 -0300

    Integração firebase e cloud firestore

--- lib/repositories/pets_repository.dart
+++ lib/repositories/pets_repository.dart
@@ -14,23 +14,23 @@ class PetsRepository {
   PetsRepository(){
     _pets.addAll([
       Pet(
-      foto: "https://avatars.githubusercontent.com/u/38079926?v=4",
+      foto: "https://avatars.githubusercontent.com/u/99221402?v=4",
       nome: "Billy", 
       tipo: "Cachorro",
 
     ),
       Pet(
-      foto: "https://avatars.githubusercontent.com/u/50676424?v=4",
+      foto: "https://avatars.githubusercontent.com/u/99221251?v=4",
       nome: "Bolt", 
       tipo: "Cachorro",
     ),
       Pet(
-      foto: "https://avatars.githubusercontent.com/u/99221052?v=4",
+      foto: "https://avatars.githubusercontent.com/u/50676424?v=4",
       nome: "Meno", 
       tipo: "Cachorro",
     ),
       Pet(
-      foto: "https://pps.whatsapp.net/v/t61.24694-24/261233290_294140406193771_647377230361946642_n.jpg?ccb=11-4&oh=242eb3e0185b8391e509a662f4a6adcd&oe=628AFD55",
+      foto: "https://avatars.githubusercontent.com/u/99221052?v=4",
       nome: "Junin", 
       tipo: "Cachorro",
     ),
